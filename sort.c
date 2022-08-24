80aac9252a6b3206fa47f3eced79f8786ed03a8e
Miller11k <miller@DESKTOP-4DD29TG.localdomain>
2022-08-24 16:25:59 -0400

    Fixed selection sort

--- sort.c
+++ sort.c
@@ -7,15 +7,15 @@ void ssort(int * arr, int size) {
 	int j;
 	int temp = 0;
 	
-	for(i = 0, i < size, i++)
+	for(i = 0; i < size; i++)
 	{
-		for(j = i + 1, j < size, j++)
+		for(j = i + 1; j < size; j++)
 		{
 			if(arr[i] < arr[j])
 			{
 				temp = arr[i];
-				a[i] = a[j];
-				a[j] = temp;
+				arr[i] = arr[j];
+				arr[j] = temp;
 			}
 		}
 	}

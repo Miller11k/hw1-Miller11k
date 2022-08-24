e798ae1f0f8d2f5505c5ecd7ee7a1fbe9fe6202c
Miller11k <miller@DESKTOP-4DD29TG.localdomain>
2022-08-24 16:05:34 -0400

    Created descending selection sort

--- sort.c
+++ sort.c
@@ -3,6 +3,21 @@
 #include "sort.h"
 
 void ssort(int * arr, int size) {
-	/* For step 3, fill this in to perform a selection sort
-	   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
+	int i;
+	int j;
+	int temp = 0;
+	
+	for(i = 0, i < size, i++)
+	{
+		for(j = i + 1, j < size, j++)
+		{
+			if(arr[i] < arr[j])
+			{
+				temp = arr[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	/*   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
 }

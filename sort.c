9a844a92013ef72e6c5bbf3af8dcf62a2153c711
Miller Joseph Kodish <mkodish@ecegrid-thin1.ecn.purdue.edu>
2022-08-25 17:09:06 -0400

    Added flag identifiers

--- sort.c
+++ sort.c
@@ -2,6 +2,7 @@
 
 #include "sort.h"
 
+#ifdef DASCENDING
 void ssort(int * arr, int size) {
 	int i;
 	int j;
@@ -19,5 +20,27 @@ void ssort(int * arr, int size) {
 			}
 		}
 	}
-	/*   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
+}
+	#endif
+
+
+#ifdef ASCENDING
+void ssort(int * arr, int size) {
+	int i;
+	int j;
+	int temp = 0;
+	
+	for(i = 0; i < size; i++)
+	{
+		for(j = i + 1; j < size; j++)
+		{
+			if(arr[i] > arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	#endif
 }

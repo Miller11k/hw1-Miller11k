36b67888ad54996ef32abc2c5a8276c6d4bf588b
Miller Joseph Kodish <mkodish@ecegrid-thin1.ecn.purdue.edu>
2022-08-25 21:49:05 -0400

    Fixed errors, final, with comments.

--- sort.c
+++ sort.c
@@ -7,12 +7,12 @@ void ssort(int * arr, int size) {
 	int i;
 	int j;
 	int temp = 0;
-	#ifdef ASCENDING
+	#ifdef ASCENDING //Determines whether to ascend one way given a specific flag identifier in main
 	for(i = 0; i < size; i++)
 	{
 		for(j = i + 1; j < size; j++)
 		{
-			if(arr[i] > arr[j])
+			if(arr[i] > arr[j]) // If the next term is smaller, switch the terms
 			{
 				temp = arr[i];
 				arr[i] = arr[j];
@@ -20,12 +20,12 @@ void ssort(int * arr, int size) {
 			}
 		}
 	}
-	#else
+	#else // Sorts the other way if the flag's condition is not met
 	for(i = 0; i < size; i++)
 	{
 		for(j = i + 1; j < size; j++)
 		{
-			if(arr[i] < arr[j])
+			if(arr[i] < arr[j]) // If the next term is larger, switch the terms
 			{
 				temp = arr[i];
 				arr[i] = arr[j];
